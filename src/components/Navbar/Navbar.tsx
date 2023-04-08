a7b7f7bdc6ee02ac7dd1af74dfe1aa177452cb76
Billy Arredondo <billy.arredondo@pucp.pe>
2023-04-08 00:24:36 -0500

    logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,25 @@
+import { AiOutlineDoubleLeft } from "react-icons/ai";
+import { GiFeatheredWing } from "react-icons/gi";
 import "./Navbar.scss";
 
 export const Navbar = () => {
-	return <aside className="nav-container"></aside>;
+	return (
+		<aside className="nav-container">
+			<h1 className="nav-container__title">
+				<div>
+					<GiFeatheredWing />
+					<span style={{ paddingLeft: "0.5rem" }}>App Name</span>
+				</div>
+				<div>
+					<AiOutlineDoubleLeft />
+				</div>
+			</h1>
+			<nav>
+				<ul className="nav-container__options">
+					<li>Opción 1</li>
+					<li>Opción 2</li>
+				</ul>
+			</nav>
+		</aside>
+	);
 };
